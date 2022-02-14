5c3ba140b8f2cb501515e1a8da97c963ebc4914d
Stiliyan St <65528270+stiliyan172@users.noreply.github.com>
2022-02-14 16:47:03 +0200

    Add files via upload

--- 14apr2021_02_darts.py
+++ 14apr2021_02_darts.py
@@ -1,7 +1,6 @@
 player_1, player_2 = input().split(', ')
 matrix_dimensions = 7
-score = {player_1: 501, player_2: 501}
-player_2_score = 501
+score_and_number_rounds = {player_1: [501, 0], player_2: [501, 0]}
 matrix = []
 
 for i in range(matrix_dimensions):
@@ -14,16 +13,29 @@ def select_player(number_rounds, player1, player2):
 
 number_of_rounds = 0
 while True:
-    number_of_rounds += 1
     row, col = [int(el) for el in eval(input())]
+    number_of_rounds += 1
     player = select_player(number_of_rounds, player_1, player_2)
     current_throw = matrix[row][col]
+    first_digit = int(matrix[row][0])
+    second_digit = int(matrix[row][-1])
+    third_digit = int(matrix[0][col])
+    fourth_digit = int(matrix[-1][col])
+    sum_4_nums = first_digit + second_digit + third_digit + fourth_digit
     if 0 > row > matrix_dimensions - 1 or 0 > col > matrix_dimensions - 1:
         continue
     else:
+        score_and_number_rounds[player][1] += 1
         if current_throw.isdigit():
-            score[player] -= int(current_throw)
+            score_and_number_rounds[player][0] -= int(current_throw)
 
         elif current_throw == "D":
-            for el in range(matrix_dimensions):
-
+            score_and_number_rounds[player][0] -= sum_4_nums * 2
+        elif current_throw == "T":
+            score_and_number_rounds[player][0] -= sum_4_nums * 3
+        elif current_throw == "B":
+            print(f'{player} won the game with {number_of_rounds} throws!')
+            break
+        if score_and_number_rounds[player][0] <= 0:
+            print(f'{player} won the game with {score_and_number_rounds[player][1]} throws!')
+            break
